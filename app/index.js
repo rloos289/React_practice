e7907f61a9d92e4de6fb130c373cbc6b54c9393a
Ryan Loos <RLoos289@gmail.com>
2016-11-15 14:44:29 -0800

    react lifecycle event

--- app/index.js
+++ app/index.js
@@ -1,15 +1,6 @@
-/*
-  Focused
-  Independent
-  Reusable
-  Small
-  Testable
-*/
-
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
-
 
 ReactDOM.render(
   routes,
